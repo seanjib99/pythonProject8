3062e393c0467bab29cbe1f3070b825072fd5310
seanjib99 <78782873+seanjib99@users.noreply.github.com>
2021-03-17 18:44:00 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# write a python program which accepts the radius of a circle from
+# the user and complete the area .(area of circle = pi+r2)
+...
+radius=int(input("enterthe radius of circle in centimenter:"))
+area_circle=(3.14*(radius * radius ))
+print(f"the area of circle is {area_circle} square water.")
+
